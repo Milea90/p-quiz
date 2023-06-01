c5c80af0bc9b1ad4ca0b471249862c85d4ad32f1
Milea90 <>
2023-06-01 18:19:27 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+#This is an introdiction to the quiz
+print("Welcome to the ultimate flowerpower-quiz - a game to enhance your knowledge of saving plants.")
+
+#This is the questions for the quiz
+question_prompts = [
+    "Your plant is hanging, what do you do?\n(A) Throw it out\n(B) Water it\n(C) Put it in the sun\n\n"
+    "Your plant is not growing, what do you do?\n(A) Give it to your mom\n(B) Give it plant nutrition\n(C) Give it more water\n\n"
+    "Your plant is getting too big, what do you do?\n(A) Cutting it down\n(B) Put it in the shadows \n(C) Give it a bigger pot\n\n"
+    "Your plant got lice, what do you do?\n(A) Kill the plant and the lice with it\n(B) Spraying the plant with soapwater\n(C) \n\n"
+    "Your plant has mold, what do you do?\n(A) Wash the plant with dishwasher\n(B) Water it\n(C) Change the soil\n\n"
+
+
+
+
+
+
+
+] 
